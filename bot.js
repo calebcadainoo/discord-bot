7b0b505a8897bf96f5f51873e51137cb74a705a7
Caleb Crosby Adainoo <code.crosby@gmail.com>
2021-10-10 22:53:56 +0000

    feat: added message event

--- bot.js
+++ bot.js
@@ -6,6 +6,16 @@ const { BOT_TOKEN } = process.env;
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log('Discord bot on duty...');
+
+  // console.log('CLIENT: ', client);
+});
+
+client.on('messageCreate', (msg) => {
+  console.log('msg: ', msg);
+  if (msg.content === 'ping') {
+    // msg.channel.send('Not tagged');
+    msg.reply('ooo you touched me...HAHA');
+  }
 });
 
 client.login(BOT_TOKEN);
